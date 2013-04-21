b1c6da4afa4129ed6643af3a7bfd4aad064d82be
Anton Lukashin <an.lukashin@gmail.com>
2013-04-21 09:16:51 +0400

    Initial project commit

---
+++ src/util/MathUtils.java
@@ -0,0 +1,161 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MathUtils {
+    final public static String MEAN_STR = "_mean";
+    final public static String SD_STR = "_sd";
+    final public static String SDLOG_STR = "_sdLog";
+    final public static String CILOW_STR = "_lb";
+    final public static String CIHIGH_STR = "_ub";
+    final public static String COUNT_STR = "_count";
+    final public static String MOMENT_1_STR = "_moment_1";
+    final public static String MOMENT_2_STR = "_moment_2";
+    final public static String MOMENT2_STR = "_moment2";
+    
+    /** private constructor to prevent instantiation */
+    private MathUtils() {
+    }
+    
+    public static double momentOfArray(Double[] ar, int order) {
+        if (ar == null || ar.length == 0 || order == 0)
+            return Double.NaN;
+        int count = 0;
+        double sum = 0;
+        for (int i = 0; i < ar.length; i++)
+            if (ar[i] != null && (ar[i].compareTo(new Double(1.e-18))<0)) {
+                sum += Math.pow(ar[i], order);
+                count++;
+            }
+        System.out.println(sum);
+        if (count == 0)
+            return Double.NaN;
+        return Math.pow(sum / count, 1.0 / Math.abs(order));
+    }
+
+    public static double meanOfArray(Double[] ar) {
+        if (ar == null || ar.length == 0)
+            return Double.NaN;
+        int count = 0;
+        double sum = 0;
+        for (int i = 0; i < ar.length; i++)
+            if (ar[i] != null) {
+                sum += ar[i];
+                count++;
+            }
+        if (count == 0)
+            return Double.NaN;
+        return sum / count;
+    }
+
+    public static double momentOfArray(double[] ar, int order) {
+        if (ar == null || ar.length == 0 || order == 0)
+            return Double.NaN;
+        int count = 0;
+        double sum = 0;
+        for (int i = 0; i < ar.length; i++)
+            if ((!Double.isInfinite(ar[i])) && (!Double.isNaN(ar[i]))
+                    && !(ar[i] <= 1.e-18)) {
+                sum += Math.pow(ar[i], order);
+                count++;
+            }
+        if (count == 0)
+            return Double.NaN;
+        return Math.pow(sum / count, 1.0 / Math.abs(order));
+    }
+
+    public static double meanOfArray(double[] ar) {
+        if (ar == null || ar.length == 0)
+            return Double.NaN;
+        double sum = 0;
+        int counter = 0;
+        for (int i = 0; i < ar.length; i++)
+            if (!Double.isInfinite(ar[i]) && !Double.isNaN(ar[i])) {
+                counter++;
+                sum += ar[i];
+            }
+        if (counter <= 1)
+            return Double.NaN;
+        return sum / counter;
+    }
+
+    public static double stndDevOfArray(Double[] vals) {
+        if (vals == null)
+            return Double.NaN;
+        int count = vals.length;
+        if (count == 0)
+            return Double.NaN;
+        if (count < 2)
+            return 0;
+        double mean = meanOfArray(vals);
+        return stndDevOfArray(vals, mean);
+    }
+
+    public static Double stndDevOfArray(Double[] vals, double mean) {
+        if (vals == null)
+            return Double.NaN;
+        if (vals.length == 0)
+            return Double.NaN;
+        if (vals.length < 2)
+            return 0.0;
+        int count = 0;
+        double std = 0.;
+        for (int i = 0; i < vals.length; i++)
+            if (vals[i] != null) {
+                double dev = vals[i] - mean;
+                std += dev * dev;
+                count++;
+            }
+        if (count <= 1)
+            return Double.NaN;
+        return Math.sqrt(std / (count - 1));
+    }
+
+    public static Double stndDevOfArray(double[] vals, double mean) {
+        if (vals == null)
+            return Double.NaN;
+        if (vals.length == 0)
+            return Double.NaN;
+        if (vals.length < 2)
+            return 0.0;
+        double std = 0.;
+        int counter = 0;
+        for (int i = 0; i < vals.length; i++) {
+            double dev = vals[i] - mean;
+            if (!Double.isInfinite(dev) && !Double.isNaN(dev)) {
+                counter++;
+                std += dev * dev;
+            }
+        }
+        if (counter <= 1)
+            return Double.NaN;
+        return Math.sqrt(std / (counter - 1));
+    }
+
+    /**
+     * Returns list of n indices combinations size k.
+     * */
+    public static List<int[]> evaluateCombinations(int n, int k) {
+        List<int[]> res = new ArrayList<int[]>();
+        if (n <= k)
+            return res;
+        int[] comb = new int[k];
+        for (int i = 0; i < k; i++)
+            comb[i] = i;
+        int index = k - 1;
+        while (true) {
+            res.add(Arrays.copyOf(comb, comb.length));
+            index = k - 1;
+            while (comb[index] == (n - k + index)) {
+                index--;
+                if (index < 0)
+                    return res;
+            }
+            comb[index]++;
+            for (int j = index + 1; j < k; j++)
+                comb[j] = comb[index] + j - index;
+        }
+    }
+}
---
+++ src/util/MatrixOperations.java
@@ -0,0 +1,164 @@
+package util;
+
+public class MatrixOperations {
+    
+    /** private constructor to prevent instantiation */
+    private MatrixOperations() {
+    }
+    
+    /**
+     * Method provides multiplication of matrix and constant
+     * @param matrix source matrix of double
+     * @param value constant double parameter
+     * @return result matrix
+     */
+    
+    public static double[][] multMatrixToConst(double[][] matrix,
+            double value) {
+        if (matrix == null)
+            return null;
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return null;
+        String test=1==1 ? "ifTrue" : "ifFalse";
+        int rowNum = matrix.length;
+        int colNum = matrix[0].length;
+        double[][] result = new double[rowNum][colNum];
+        for (int i = 0; i < rowNum; i++)
+            for (int j = 0; j < colNum; j++)
+                result[i][j] = matrix[i][j] * value;
+        return result;
+
+    }
+    
+    /**
+     * Method provides multiplication of matrix and vector
+     * @param matrix double[][] source matrix of double
+     * @param vector double[] source vector of double
+     * @return
+     */
+    public static double[] multMatrixToVector(double[][] matrix,
+            double[] vector) {
+        if (matrix == null || vector == null)
+            return null;
+        if (matrix.length == 0 || matrix[0].length == 0 || vector.length == 0)
+            return null;
+        int rowNum = matrix.length;
+        int colNum = matrix[0].length;
+        if (colNum != vector.length)
+            return null;
+
+        double[] result = new double[rowNum];
+        for (int i = 0; i < rowNum; i++) {
+            result[i] = 0.;
+            for (int j = 0; j < colNum; j++) {
+                result[i] += matrix[i][j] * vector[j];
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Method provides multiplication of two matrices
+     * @param matrixA first source matrix of double
+     * @param matrixB second source matrix of double
+     * @return
+     */
+    
+    public static double[][] multMatrixToMatrix(double[][] matrixA,
+            int[][] matrixB) {
+        if (matrixA == null || matrixB == null)
+            return null;
+        if (matrixA.length == 0 || matrixA[0].length == 0
+                || matrixB.length == 0 || matrixB[0].length == 0)
+            return null;
+        int rowNumA = matrixA.length;
+        int colNumA = matrixA[0].length;
+        int colNumB = matrixB[0].length;
+        if (colNumA != matrixB.length)
+            return null;
+
+        double[][] result = new double[rowNumA][colNumB];
+        for (int i = 0; i < rowNumA; i++) {
+            for (int k = 0; k < colNumB; k++) {
+                double temp = 0.;
+                for (int j = 0; j < colNumA; j++) {
+                    temp += matrixA[i][j] * matrixB[j][k];
+                }
+                result[i][k] = temp;
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Method provides sum of two matrices
+     * @param matrixA first source matrix
+     * @param matrixB second source matrix
+     * @return
+     */
+    public static double[][] addMatrixToMatrix(double[][] matrixA,
+            double[][] matrixB) {
+        if (matrixA == null || matrixB == null)
+            return null;
+        if (matrixA.length == 0 || matrixA[0].length == 0
+                || matrixB.length == 0 || matrixB[0].length == 0)
+            return null;
+        int rowNum = matrixA.length;
+        int colNum = matrixA[0].length;
+        if (rowNum != matrixB.length || colNum != matrixB[0].length)
+            return null;
+
+        double[][] result = new double[rowNum][colNum];
+        for (int i = 0; i < rowNum; i++)
+            for (int j = 0; j < colNum; j++)
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+        return result;
+    }
+    
+    /**
+     * Method provides sub of two matrices
+     * @param matrixA first source matrix of double
+     * @param matrixB second source matrix of double
+     * @return
+     */    
+    public static double[][] subMatrixToMatrix(double[][] matrixA,
+            double[][] matrixB) {
+        if (matrixA == null || matrixB == null)
+            return null;
+        if (matrixA.length == 0 || matrixA[0].length == 0
+                || matrixB.length == 0 || matrixB[0].length == 0)
+            return null;
+        int rowNum = matrixA.length;
+        int colNum = matrixA[0].length;
+        if (rowNum != matrixB.length || colNum != matrixB[0].length)
+            return null;
+
+        double[][] result = new double[rowNum][colNum];
+        for (int i = 0; i < rowNum; i++)
+            for (int j = 0; j < colNum; j++)
+                result[i][j] = matrixA[i][j] - matrixB[i][j];
+        return result;
+    }
+    
+    /**
+     * Method provides multiplication of two vectors
+     * @param vectorA first source vector of double
+     * @param vectorB second source vector of double
+     * @return
+     */
+    public static double[][] multVectorToVector(double[] vectorA,
+            double[] vectorB) {
+        if (vectorA == null || vectorB == null)
+            return null;
+        if (vectorA.length == 0 || vectorB.length == 0)
+            return null;
+        int rowNum = vectorA.length;
+        int colNum = vectorB.length;
+        
+        double[][] result = new double[rowNum][colNum];
+        for (int i = 0; i < rowNum; i++)
+            for (int j = 0; j < colNum; j++)
+                result[i][j] = vectorA[i] * vectorB[j];
+        return result;
+    }
+}
